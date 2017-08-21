cab562a1335d83787c2a59b12e4b8180cb69cd65
Andrey Zhuk <azhuk@alaskartech.com>
2017-08-21 17:09:39 +0300

    Правки

--- install/sql/alkoid.sql
+++ install/sql/alkoid.sql
@@ -33,7 +33,7 @@ COMMENT ON COLUMN propvalue.val_dec IS 'вещественное, два сим�
 -- устанавливаем тип свойства "Цена" decimal
 UPDATE paramvalue SET value = 'decimal' WHERE id_propgroup = 1 AND n_propgroup = 1 AND n_proptype = 1;
 
--- добавляем свойсво количество
+-- добавляем свойсво количество товара в наличии
 INSERT INTO property (id_propgroup, n, id_proptype, name, visible) VALUES (1, 8, 1, 'Qty', true);
 INSERT INTO paramvalue (id_propgroup, n_propgroup, id_proptype, n_proptype, value) VALUES (1, 8, 1, 1, 'integer');
 
