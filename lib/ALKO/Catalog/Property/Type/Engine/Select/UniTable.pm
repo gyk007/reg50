4ae50a5a056cb1d5c8cdd7dd97ea423eda8d43ce
Andrey Zhuk <azhuk@alaskartech.com>
2017-09-11 18:15:43 +0300

    Таблица propdata

--- lib/ALKO/Catalog/Property/Type/Engine/Select/UniTable.pm
+++ lib/ALKO/Catalog/Property/Type/Engine/Select/UniTable.pm
@@ -21,8 +21,8 @@ my $propdata = ALKO::Catalog::Property::Data->All->Hash('id_propgroup');
 
 # Хэш: $prop_extra = {id_propgroup}{n} = ['made_in', ...];
 my $prop_extra;
-for my $id_propgroup (keys $propdata) {
-	push $prop_extra->{$id_propgroup}{$_->{n_property}}, $_->{extra} for (@{$propdata->{$id_propgroup}});
+while( my( $id_propgroup, $value ) = each %{$propdata} ){
+	push $prop_extra->{$id_propgroup}{$_->{n_property}}, $_->{extra} for (@$value});
 }
 
 =begin nd
