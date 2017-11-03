4a3fd4f14bf01865fa961712786ee00df336c524
Andrey Zhuk <azhuk@alaskartech.com>
2017-11-03 12:15:45 +0300

    SEND MAIL

--- public_html/index.pl
+++ public_html/index.pl
@@ -8,8 +8,9 @@ use warnings;
 
 use WooF::Debug;
 use ALKO::Server;
+use ALKO::SendMail qw(send_mail);
 
-my $Server = ALKO::Server->new;
+my $Server = ALKO::Server->new(output_t => 'JSON');
 
 # Простейший обработчик. Клиенту отдается статичный шаблон, в лог веб-сервера - версия постгрис.
 # URL: /
@@ -23,5 +24,43 @@ $Server->add_handler(DEFAULT => {
 		OK;
 	},
 });
+
+# Получить данные представителя магазина
+#
+# GET
+# URL: /?
+#   action = send_mail
+#   name   = string
+#   email  = string
+#   phone  = string
+#
+$Server->add_handler(SEND_MAIL => {
+	input => {
+		allow => ['action', 'name', 'email', 'phone'],
+	},
+	call => sub {
+		my $S = shift;
+		my ($I, $O) = ($S->I, $S->O);
+
+		send_mail({
+			template => 'new_client',
+			to       => 'grd77@bis100.ru',
+			subject  => 'REG50 Регистрация',
+			info     => $I
+		});
+
+		OK;
+	},
+});
+
+$Server->dispatcher(sub {
+	my $S = shift;
+	my $I = $S->I;
+
+	return ['SEND_MAIL']     if exists $I->{action} and $I->{action} eq 'send_mail';
+
+	['DEFAULT'];
+});
+
 
 $Server->listen;
